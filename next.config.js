2d2677615dc1d49c51ac6c122226a3558add9838
Louis <louisboucherie@protonmail.com>
2025-01-27 23:44:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true, // Disable linting during builds
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Skip type-checking (even without TypeScript)
+  },
 };
 
 module.exports = nextConfig;
